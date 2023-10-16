355fad94a7c3b0951d87a63a710ad2ba3bda41b1
EgorGER <QwertiQQQ@gmail.com>
2023-10-16 19:54:10 +0300

    добавил генератор сообщений

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+const name = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+
+const mesage = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
 const object = 4; //количество объектов
 
 // функция случайного числа
@@ -8,16 +28,38 @@ const getRandomInteger = (a, b) => {
   return Math.floor(result);
 };
 
-//заполняем массив обектами
+//случайный элемент массива
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+//создает комментарий
+const createСomments = (x, ID) => ({
+  id: ID+1,
+  avatar: 'img/avatar-' + getRandomInteger(1, 6) + '.svg',
+  message: getRandomArrayElement(mesage),
+  name: getRandomArrayElement(name),
+});
+
+//Генерирует массив с комментариями
+const generatesСomments = Array.from({length: getRandomInteger(0,30)}, createСomments);
+console.log(generatesСomments);
+
+//создает обектами
 const createObject = (x, ID) => ({
   id: ID+1,
   url: 'photos/' + (ID+1) + '.jpg',
   description: 'булочки',
   likes:  getRandomInteger(15, 200),
-  comments: 'ТУТ',
-
+  comments: generatesСomments,
 });
-//Генерирует объекты
+
+//Генерирует массив с объектами
 const generatesObject = Array.from({length: object}, createObject);
 
-console.log(generatesObject);
+
+
+
+
+
+
+
+
