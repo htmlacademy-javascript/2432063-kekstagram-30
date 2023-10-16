e88ebbf2a2b2c707151fb0474d78047c7a67f941
EgorGER <QwertiQQQ@gmail.com>
2023-10-16 19:01:41 +0300

    генерирует обекты

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const object = 4; //количество обектов
+
+// функция случайного числа
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+//заполняем массив коментариев
+const createComments = (x, ID) => ({
+  id: ID+1,
+  url: 'photos/' + (ID+1) + '.jpg',
+  description: 'булочки',
+  likes:  getRandomInteger(15, 200),
+  comments: 'ТУТ',
+
+});
+//Генерирует объекты
+const generatesObject = Array.from({length: object}, createComments);
+
+console.log(generatesObject);
