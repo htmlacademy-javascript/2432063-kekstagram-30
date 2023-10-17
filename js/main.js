38a3effc0c4301e716444549e2bfc8495e82ab3b
EgorGER <QwertiQQQ@gmail.com>
2023-10-17 17:24:58 +0300

    кореректирует имена переменных, убирате переменную х

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const names = [
+const NAMES = [
   'Иван',
   'Хуан Себастьян',
   'Мария',
@@ -9,7 +9,7 @@ const names = [
   'Вашингтон',
 ];
 
-const messages = [
+const MASSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -18,7 +18,7 @@ const messages = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-const photoCount = 4; //количество объектов
+const PHOTO_COUNT = 4; //количество объектов
 
 // функция случайного числа
 const getRandomInteger = (a, b) => {
@@ -32,11 +32,11 @@ const getRandomInteger = (a, b) => {
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 //создает комментарий
-const createСomment = (x, ID) => ({
-  id: ID + 1,
+const createСomment = (undefined, id) => ({
+  id: id + 1,
   avatar: `img/avatar-${ getRandomInteger(1, 6) }.svg`,
-  message: getRandomArrayElement(messages),
-  name: getRandomArrayElement(names),
+  message: getRandomArrayElement(MASSAGES),
+  name: getRandomArrayElement(NAMES),
 });
 
 //Генерирует массив с комментариями
@@ -44,17 +44,15 @@ const generatesСomments = Array.from({length: getRandomInteger(0,30)}, createС
 
 
 //создает объект
-const createObject = (x, ID) => ({
-  id: ID + 1,
-  url: `photos/${ ID + 1 }.jpg`,
+const createObject = (undefined, id) => ({
+  id: id + 1,
+  url: `photos/${ id + 1 }.jpg`,
   description: 'булочки',
   likes:  getRandomInteger(15, 200),
   comments: generatesСomments,
 });
 
 //Генерирует массив с объектами
-const generatesObject = Array.from({length: photoCount}, createObject);
+const generatesObject = Array.from({length: PHOTO_COUNT}, createObject);
 
 console.log(generatesObject);
-
-
