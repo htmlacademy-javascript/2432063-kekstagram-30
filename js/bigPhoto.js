136827328a5316e52440f81365ad1e9e2cc72f8a
EgorGER <QwertiQQQ@gmail.com>
2023-11-01 20:08:15 +0300

    добоаляте количество лайков и коментариев к большому фото

--- js/bigPhoto.js
+++ js/bigPhoto.js
@@ -4,6 +4,8 @@ import { photosList } from './gallery';
 const bigPhoto = document.querySelector('.big-picture');
 const miniPhoto = photosList.querySelectorAll('.picture');
 const closeButton = bigPhoto.querySelector('.big-picture__cancel');
+const bigPhotoSocial = document.querySelector('.big-picture__social');
+const socialCommentCount = document.querySelector('.social__comment-count');
 
 
 const openBigPhoto = () => {
@@ -18,6 +20,10 @@ miniPhoto.forEach((photosList) => {
   photosList.addEventListener('click', (evt) => {
     evt.preventDefault();
     openBigPhoto();
+    bigPhoto.querySelector('img').src = photosList.querySelector('.picture__img').src;
+    bigPhotoSocial.querySelector('.likes-count').textContent = photosList.querySelector('.picture__likes').textContent;
+    socialCommentCount.querySelector('.social__comment-shown-count').textContent = 2; //не понял что сюда надо вставить
+    socialCommentCount.querySelector('.social__comment-total-count').textContent = photosList.querySelector('.picture__comments').textContent;
 
   });
 });
