88cddac2c95178832424457e40a9285d1dc0676c
EgorGER <QwertiQQQ@gmail.com>
2023-11-03 19:58:22 +0300

    отрисовывыет коментарии на странице

--- js/bigPhoto.js
+++ js/bigPhoto.js
@@ -1,5 +1,5 @@
 import { photosList } from './gallery';
-import { socialComments } from './Comment.js';
+import { renderMessagesByIndex } from './Comment.js';
 
 const bigPhoto = document.querySelector('.big-picture');
 const miniPhoto = photosList.querySelectorAll('.picture');
@@ -10,21 +10,31 @@ const socialCommentCount = document.querySelector('.social__comment-count');
 
 const openBigPhoto = () => {
   bigPhoto.classList.remove('hidden');
+
+  document.querySelector('body').classList.add('modal-open');
+  document.querySelector('.social__comment-count').classList.add('hidden');
+  document.querySelector('.comments-loader').classList.add('hidden');
+
 };
 
 const closeBigPhoto = () => {
   bigPhoto.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  document.querySelector('.social__comment-count').classList.remove('hidden');
+  document.querySelector('.comments-loader').classList.remove('hidden');
+
 };
 
-miniPhoto.forEach((photosList) => {
-  photosList.addEventListener('click', (evt) => {
+miniPhoto.forEach((selectedPhoto, index) => {
+  selectedPhoto.addEventListener('click', (evt) => {
     evt.preventDefault();
-    openBigPhoto();
-    bigPhoto.querySelector('img').src = photosList.querySelector('.picture__img').src;
-    bigPhotoSocial.querySelector('.likes-count').textContent = photosList.querySelector('.picture__likes').textContent;
+    bigPhoto.querySelector('img').src = selectedPhoto.querySelector('.picture__img').src;
+    bigPhotoSocial.querySelector('.likes-count').textContent = selectedPhoto.querySelector('.picture__likes').textContent;
     socialCommentCount.querySelector('.social__comment-shown-count').textContent = 2; //не понял что сюда надо вставить
-    socialCommentCount.querySelector('.social__comment-total-count').textContent = photosList.querySelector('.picture__comments').textContent;
-
+    socialCommentCount.querySelector('.social__comment-total-count').textContent = selectedPhoto.querySelector('.picture__comments').textContent;
+    bigPhotoSocial.querySelector('.social__caption').textContent = selectedPhoto.querySelector('.picture__img').alt;
+    renderMessagesByIndex(index);
+    openBigPhoto(index);
   });
 });
 
