2d83bee7384547ec1b7c2e807cc149f053801cd8
EgorGER <QwertiQQQ@gmail.com>
2023-11-08 23:23:35 +0300

    Добовлят отрисовку коментарив по 5шт.

--- js/bigPhoto.js
+++ js/bigPhoto.js
@@ -7,6 +7,7 @@ const miniPhoto = photosList.querySelectorAll('.picture');
 const closeButton = bigPhoto.querySelector('.big-picture__cancel');
 const bigPhotoSocial = document.querySelector('.big-picture__social');
 const socialCommentCount = document.querySelector('.social__comment-count');
+const socialCommentsLoader = document.querySelector('.social__comments-loader');
 
 
 const onEscapeKeydown = () => {
@@ -15,6 +16,8 @@ const onEscapeKeydown = () => {
       evt.preventDefault();
       bigPhoto.classList.add('hidden');
       document.querySelector('body').classList.remove('modal-open');
+      document.querySelector('.social__comment-count').classList.remove('hidden');
+      document.querySelector('.comments-loader').classList.remove('hidden');
     }
   });
 };
@@ -22,8 +25,8 @@ const onEscapeKeydown = () => {
 const openBigPhoto = () => {
   bigPhoto.classList.remove('hidden');
   document.querySelector('body').classList.add('modal-open');
-  document.querySelector('.social__comment-count').classList.add('hidden');
-  document.querySelector('.comments-loader').classList.add('hidden');
+  //document.querySelector('.social__comment-count').classList.add('hidden');
+  // document.querySelector('.comments-loader').classList.add('hidden');
   onEscapeKeydown();
 };
 
@@ -34,12 +37,39 @@ const closeBigPhoto = () => {
   document.querySelector('.comments-loader').classList.remove('hidden');
 };
 
+
 miniPhoto.forEach((selectedPhoto, index) => {
   selectedPhoto.addEventListener('click', (evt) => {
     evt.preventDefault();
+
+    const commentTotalCount = Number(selectedPhoto.querySelector('.picture__comments').textContent);
+    let countReanderComment = 5;
+    if (commentTotalCount <= countReanderComment) {
+      countReanderComment = commentTotalCount;
+      document.querySelector('.comments-loader').classList.add('hidden');
+    }
+
+    socialCommentsLoader.addEventListener('click', () => {
+
+      if (commentTotalCount <= 5) {
+        socialCommentCount.querySelector('.social__comment-shown-count').textContent = commentTotalCount;
+
+      } if (countReanderComment + 5 > commentTotalCount) {
+        socialCommentCount.querySelector('.social__comment-shown-count').textContent = commentTotalCount;
+        document.querySelector('.comments-loader').classList.add('hidden');
+        countReanderComment = 5;
+
+      } else {
+        countReanderComment += 5;
+        socialCommentCount.querySelector('.social__comment-shown-count').textContent = countReanderComment;
+
+      }
+    });
+
+
     bigPhoto.querySelector('img').src = selectedPhoto.querySelector('.picture__img').src;
     bigPhotoSocial.querySelector('.likes-count').textContent = selectedPhoto.querySelector('.picture__likes').textContent;
-    socialCommentCount.querySelector('.social__comment-shown-count').textContent = 2; //не понял что сюда надо вставить
+    socialCommentCount.querySelector('.social__comment-shown-count').textContent = countReanderComment;
     socialCommentCount.querySelector('.social__comment-total-count').textContent = selectedPhoto.querySelector('.picture__comments').textContent;
     bigPhotoSocial.querySelector('.social__caption').textContent = selectedPhoto.querySelector('.picture__img').alt;
     renderMessagesByIndex(index);
