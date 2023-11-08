b3c74d67820fe453d172ae5940e60eb9e4c98027
Keks <keks@htmlacademy.ru>
2023-11-08 18:31:29 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/Comment.js
+++ 9/js/Comment.js
@@ -7,19 +7,13 @@ const renderMessagesByIndex = (index) => {
   const comments = curentPhoto.comments;
   const createListPictures = document.createDocumentFragment();
 
-  const messageTemplate = document.createElement('li');
+  const messageTemplate = document.querySelector('.social__comment');
   const socialComments = document.querySelector('.social__comments');
 
-  messageTemplate.innerHTML = `
-<img class="social__picture" src="" alt="" width="35" height="35">
-<p class="social__text">{{текст комментария}}</p>
-`;
-
-  comments.forEach((coment) => {
+   comments.forEach((coment) => {
     const {avatar, message, name} = coment;
 
     const element = messageTemplate.cloneNode(true);
-    element.setAttribute('class', 'social__comment');
     element.querySelector('.social__picture').src = avatar;
     element.querySelector('.social__picture').alt = name;
     element.querySelector('.social__text').textContent = message;
