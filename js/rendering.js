46e7c8f6ce72d187166b4702ef03c1dcc59daa10
EgorGER <QwertiQQQ@gmail.com>
2023-11-18 14:10:22 +0300

    module11-task1

---
+++ js/rendering.js
@@ -0,0 +1,25 @@
+import { renderThumbnails } from './gallery';
+import { showPicture } from './bigPhoto';
+
+const container = document.querySelector('.pictures');
+
+const renderGallery = (pictures) => {
+  container.addEventListener('click', (evt) => {
+    const thumbnail = evt.target.closest('[data-thumbnail-id]');
+
+    if (!thumbnail) {
+      return;
+    }
+
+    evt.preventDefault();
+
+    const thumbnailId = +thumbnail.dataset.thumbnailId;
+    const pictureData = pictures.find(({ id }) => id === thumbnailId);
+
+    showPicture(pictureData);
+  });
+
+  renderThumbnails(pictures, container);
+};
+
+export { renderGallery };
