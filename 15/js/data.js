759616eff16e08af8a5e48b31e3efffb27677d29
Keks <keks@htmlacademy.ru>
2023-11-23 18:27:52 +0300

    :heavy_check_mark: Сборка #15

---
+++ 15/js/data.js
@@ -0,0 +1,73 @@
+import { loadPictures } from './api.js';
+import { debounce, showErrorMessage } from './util.js';
+import { initGalleryFilters } from './filter.js';
+import { renderPhotos } from './gallery.js';
+
+const filterForm = document.querySelector('.img-filters__form');
+const defaultButton = filterForm.querySelector('#filter-default');
+const randomButton = filterForm.querySelector('#filter-random');
+const discussedButton = filterForm.querySelector('#filter-discussed');
+
+const bootstrap = async () => {
+  try {
+    const pictures = await loadPictures();
+    return pictures;
+  } catch (error) {
+    showErrorMessage();
+  }
+};
+
+const photos = await bootstrap();
+
+const rePaintDefault = () => {
+  const pictures = document.querySelectorAll('.picture');
+  pictures.forEach((item) => item.remove());
+  const elements = initGalleryFilters(photos, 'default');
+  const imageFilterButtonActive = filterForm.querySelector('.img-filters__button--active');
+  imageFilterButtonActive.classList.remove('img-filters__button--active');
+  defaultButton.classList.add('img-filters__button--active');
+  renderPhotos(elements);
+};
+const rePaintRandom = () => {
+  const pictures = document.querySelectorAll('.picture');
+  pictures.forEach((item) => item.remove());
+  const elements = initGalleryFilters(photos, 'random');
+  const imageFilterButtonActive = filterForm.querySelector('.img-filters__button--active');
+  imageFilterButtonActive.classList.remove('img-filters__button--active');
+  randomButton.classList.add('img-filters__button--active');
+  renderPhotos(elements);
+};
+const rePaintDiscussed = () => {
+  const pictures = document.querySelectorAll('.picture');
+  pictures.forEach((item) => item.remove());
+  const elements = initGalleryFilters(photos, 'discussed');
+  const imageFilterButtonActive = filterForm.querySelector('.img-filters__button--active');
+  imageFilterButtonActive.classList.remove('img-filters__button--active');
+  discussedButton.classList.add('img-filters__button--active');
+
+  renderPhotos(elements);
+  console.log(elements);
+};
+
+const debounceDefault = debounce(rePaintDefault);
+const debounceRandom = debounce(rePaintRandom);
+const debounceDiscussed = debounce(rePaintDiscussed);
+
+
+const filteredPhotos = initGalleryFilters(photos, 'default');
+renderPhotos(filteredPhotos);
+
+defaultButton.addEventListener('click', () => {
+  debounceDefault();
+});
+
+randomButton.addEventListener('click', () => {
+  debounceRandom();
+});
+
+
+discussedButton.addEventListener('click', () => {
+  debounceDiscussed();
+});
+
+export {filteredPhotos};
---
+++ 15/js/util.js
@@ -0,0 +1,36 @@
+const REMOVE_MESSAGE_TIMEOUT = 5000;
+
+const errorMessageTemplate = document
+  .querySelector('#data-error')
+  .content
+  .querySelector('.data-error');
+
+const showErrorMessage = () => {
+  const errorElement = errorMessageTemplate.cloneNode(true);
+  document.body.append(errorElement);
+
+  setTimeout(() => {
+    errorElement.remove();
+  }, REMOVE_MESSAGE_TIMEOUT);
+};
+
+// функция случайного числа
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+function debounce(callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+export {isEscapeKey, showErrorMessage, debounce};
+
