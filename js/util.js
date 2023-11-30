f6f4e49b9d48512395469d864b29ed31477c7221
EgorGER <QwertiQQQ@gmail.com>
2023-11-30 19:01:43 +0300

    стрелочный дебаунс

--- js/util.js
+++ js/util.js
@@ -19,13 +19,13 @@ const getRandomIndex = (min, max) => Math.floor(Math.random() * (max - min));
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-function debounce(callback, timeoutDelay = 500) {
+const debounce = (callback, timeoutDelay = 500) => {
   let timeoutId;
   return (...rest) => {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
   };
-}
+};
 
 export {isEscapeKey, showErrorMessage, debounce, getRandomIndex};
 
